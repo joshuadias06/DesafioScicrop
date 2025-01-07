83f841ea8159b272127832be06553a4565e03952
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-07 20:48:06 -0300

    Created PostForm

--- agropop/scicrop/views.py
+++ agropop/scicrop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 def list_posts(request):
     posts = Post.objects.all()
     return  render(request, 'posts/index.html', {'posts': posts})
+
+
