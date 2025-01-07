a974cdc85bf13735a0d0f386e61a773c4a7541d8
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-07 20:42:54 -0300

    Created Read -> list_posts

--- agropop/scicrop/views.py
+++ agropop/scicrop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def list_posts(request):
+    posts = Post.objects.all()
+    return  render(request, 'posts/index.html', {'posts': posts})
