e6f82d5334d6bc930721b0656bd87d7866d8ffd5
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-07 21:22:33 -0300

    Create -> add_post

--- agropop/scicrop/views.py
+++ agropop/scicrop/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+#IMPORT's
+from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+#CRUD DOS POST's (C -> CREATE, R -> READ, U -> UPDATED, D -> DELETE )
 
+#R -> READ
 def list_posts(request):
     posts = Post.objects.all()
     return  render(request, 'posts/index.html', {'posts': posts})
+
+#C -> CREATE
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_posts)
+        else:
+            form = PostForm()
+        return render(request, 'posts/add_post.html', {'form': form})
 
 
