58a7347453faba1c63ddf081134f669053b1eac9
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-09 19:05:23 -0300

    Updated urls.py

--- agropop/scicrop/urls.py
+++ agropop/scicrop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.list_posts, name='list_posts'),  # URL para listagem de posts
     path('post/<str:title>/', views.post_detail, name='post_detail'),  # URL para detalhe do post
     path('add/', views.add_post, name='add_post'),  # URL para criar um novo post
-    path('edit_post/<str:title>/', edit_post, name='edit_post'),
+    path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
     path('delete_post/<str:title>/', delete_post, name='delete_post'),
 ]
