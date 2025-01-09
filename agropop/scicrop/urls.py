00064c57fd261436a53fb0036a08b7063a4ce7ea
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-08 22:29:25 -0300

    Updated urls.py

--- agropop/scicrop/urls.py
+++ agropop/scicrop/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import edit_post, delete_post
 
 urlpatterns = [
     path('', views.list_posts, name='list_posts'),  # URL para listagem de posts
     path('post/<str:title>/', views.post_detail, name='post_detail'),  # URL para detalhe do post
     path('add/', views.add_post, name='add_post'),  # URL para criar um novo post
+    path('edit_post/<str:title>/', edit_post, name='edit_post'),
+    path('delete_post/<str:title>/', delete_post, name='delete_post'),
 ]
